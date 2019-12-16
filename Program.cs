355d52db804e15f31be050c7e927312dfbfa05d3
Janick Eicher <eicherjanick@gmail.com>
2019-12-16 11:19:10 +0100

    Experimenting around with VS-Git

--- Program.cs
+++ Program.cs
@@ -2,11 +2,11 @@
 using System;
 using System.Net.Sockets;
 
+//This Program is the Main Menu, for now
 namespace Chat
 {
     class Program
     {
-
         static void Main(string[] args)
         {
             String start = Console.ReadLine();
